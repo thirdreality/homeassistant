5274327a0bf5b364c5aec63d8f38fa0ce7d395d5
jintj <jintj@3reality.com>
2024-08-22 11:31:32 +0800

    update py

--- ZHA/temperature_humidity.py
+++ ZHA/temperature_humidity.py
@@ -1,23 +1,19 @@
 """Third Reality Temperature humidity devices."""
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-import zigpy.types as t
-from zigpy.zcl.clusters.general import Basic, Ota, PowerConfiguration
-from zigpy.zcl.clusters.measurement import TemperatureMeasurement ,RelativeHumidity
-from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
-from zhaquirks import CustomCluster
-
+from typing import Final
+from zigpy.profiles import zha 
+from zigpy.quirks import CustomDevice 
+import zigpy.types as t 
+from zigpy.zcl.clusters.general import Basic, Ota, PowerConfiguration 
+from zigpy.zcl.clusters.measurement import TemperatureMeasurement ,RelativeHumidity 
+from zhaquirks import CustomCluster 
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef 
 from zigpy.zcl.clusters.general import (
     Basic,
-    GreenPowerProxy,
-    Groups,
     Identify,
-    OnOff,
     Ota,
-    Scenes,
 )
 
-from zhaquirks.const import (
+from zhaquirks.const import ( 
     DEVICE_TYPE,
     ENDPOINTS,
     INPUT_CLUSTERS,
@@ -25,20 +21,41 @@ from zhaquirks.const import (
     OUTPUT_CLUSTERS,
     PROFILE_ID,
 )
-from zhaquirks.thirdreality import THIRD_REALITY
+from zhaquirks.thirdreality import THIRD_REALITY 
 
-MANUFACTURER_SPECIFIC_CLUSTER_ID = 0xFF01
+THIRDREALITY_CLUSTER_ID = 0xFF01
 
 
-class ThirdRealityAccelCluster(CustomCluster):
+class ControlMode(t.int16s):  # noqa: D101
+
+    pass
+
+class ThirdRealityCluster(CustomCluster):
     """ThirdReality Temperature humidity Cluster."""
 
-    cluster_id = MANUFACTURER_SPECIFIC_CLUSTER_ID
-    attributes = {
-        0x0030: ("LCD_display_off", t.uint8_t, True),
-        0x0031: ("Temperature_calibration", t.int16s, True),
-        0x0032: ("humidity_calibration", t.int16s, True),
-    }
+    cluster_id = THIRDREALITY_CLUSTER_ID
+
+    class AttributeDefs(BaseAttributeDefs): 
+        """Attribute definitions."""
+
+        Celsius_degree_calibration: Final = ZCLAttributeDef( 
+            id=0x0031,
+            type=ControlMode,
+            is_manufacturer_specific=True,
+        )
+
+        humidity_calibration: Final = ZCLAttributeDef(
+            id=0x0032,
+            type=ControlMode,
+            is_manufacturer_specific=True,
+        )
+
+        Fahrenheit_degree_calibration: Final = ZCLAttributeDef( 
+            id=0x0033,
+            type=ControlMode,
+            is_manufacturer_specific=True,
+        )
+  
 
 
 class Temperature_humidity(CustomDevice):
@@ -57,7 +74,7 @@ class Temperature_humidity(CustomDevice):
                     #PollControl.cluster_id,
                     TemperatureMeasurement.cluster_id,
                     RelativeHumidity.cluster_id,
-                    ThirdRealityAccelCluster.cluster_id,
+                    ThirdRealityCluster.cluster_id,
                 ],
                 OUTPUT_CLUSTERS: [
                     Ota.cluster_id,
@@ -77,7 +94,7 @@ class Temperature_humidity(CustomDevice):
                    # PollControl.cluster_id,
                     TemperatureMeasurement.cluster_id,
                     RelativeHumidity.cluster_id,
-                    ThirdRealityAccelCluster,
+                    ThirdRealityCluster,
                 ],
                 OUTPUT_CLUSTERS: [
                     Ota.cluster_id,
@@ -100,10 +117,10 @@ class Temperature_humidity_lite(CustomDevice):
                     Basic.cluster_id,
                     PowerConfiguration.cluster_id,
                     Identify.cluster_id,
-                    PollControl.cluster_id,
+                    #PollControl.cluster_id, # type: ignore
                     TemperatureMeasurement.cluster_id,
                     RelativeHumidity.cluster_id,
-                    ThirdRealityAccelCluster.cluster_id,
+                    ThirdRealityCluster.cluster_id,
                 ],
                 OUTPUT_CLUSTERS: [
                     Ota.cluster_id,
@@ -120,10 +137,52 @@ class Temperature_humidity_lite(CustomDevice):
                     Basic.cluster_id,
                     PowerConfiguration.cluster_id,
                     Identify.cluster_id,
-                    PollControl.cluster_id,
+                    #PollControl.cluster_id, # type: ignore
                     TemperatureMeasurement.cluster_id,
                     RelativeHumidity.cluster_id,
-                    ThirdRealityAccelCluster,
+                    ThirdRealityCluster,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,
+                ],
+            }
+        },
+    }
+
+
+class Soil_Moisture_Sensor(CustomDevice):
+    """ThirdReality Soil Moisture Sensor device."""
+
+    signature = {
+        MODELS_INFO: [(THIRD_REALITY, "3RSM0147Z")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.TEMPERATURE_SENSOR,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    RelativeHumidity.cluster_id,
+                    ThirdRealityCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,
+                ],
+            }
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.TEMPERATURE_SENSOR,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    RelativeHumidity.cluster_id,
+                    ThirdRealityCluster,
                 ],
                 OUTPUT_CLUSTERS: [
                     Ota.cluster_id,
