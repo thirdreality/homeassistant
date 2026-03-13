711af54279bd5d795ebf80339f3fafd766d15de4
jintj <jintj@3reality.com>
2026-03-13 18:00:41 +0800

    update

--- ZHA(quirks v2)/Multi-Function Smart Presence Sensor R3.py
+++ ZHA(quirks v2)/Multi-Function Smart Presence Sensor R3.py
@@ -1,44 +1,86 @@
-"""Third Reality formaldehyde sensor devices."""
+"""Third Reality 60G radar devices."""
 
 from typing import Final
 
 from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import QuirkBuilder, ReportingConfig, SensorDeviceClass, SensorStateClass
+from zigpy.quirks.v2 import QuirkBuilder, SensorDeviceClass, SensorStateClass
 from zigpy.quirks.v2.homeassistant import CONCENTRATION_PARTS_PER_BILLION
 import zigpy.types as t
 from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
-from zigpy.zcl.clusters.measurement import FormaldehydeConcentration
-
-from zhaquirks.tuya.builder import TuyaFormaldehydeConcentration
-from zhaquirks.develco.air_quality import DevelcoVOCMeasurement
 
 
 class ThirdRealityRadarCluster(CustomCluster):
-    """Third Reality's plug private cluster."""
+    """Third Reality's 60G radar private cluster."""
 
-    cluster_id = 0x042e
+    cluster_id = 0x042E
 
     class AttributeDefs(BaseAttributeDefs):
         """Define the attributes of a private cluster."""
 
-        # reset the accumulated power of the plug
-        volatile_organic_compounds: Final = ZCLAttributeDef(
+        # measure the value of voc
+        total_volatile_organic_compounds: Final = ZCLAttributeDef(
             id=0x0000,
             type=t.Single,
             is_manufacturer_specific=True,
         )
+
+        tvoc_sensor_calibration: Final = ZCLAttributeDef(
+            id=0xF001,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
+
+        sensor_sensitivity: Final = ZCLAttributeDef(
+            id=0xF002,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
+        
+        air_threshold: Final = ZCLAttributeDef(
+            id=0xF003,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+
 
 (
     QuirkBuilder("Third Reality, Inc", "3RPL01084Z")
     .replaces(ThirdRealityRadarCluster)
     .sensor(
         endpoint_id=1,
-        attribute_name=ThirdRealityRadarCluster.AttributeDefs.volatile_organic_compounds.name,
+        attribute_name=ThirdRealityRadarCluster.AttributeDefs.total_volatile_organic_compounds.name,
         cluster_id=ThirdRealityRadarCluster.cluster_id,
         device_class=SensorDeviceClass.VOLATILE_ORGANIC_COMPOUNDS_PARTS,
         state_class=SensorStateClass.MEASUREMENT,
         unit=CONCENTRATION_PARTS_PER_BILLION,
-        fallback_name="Volatile organic compounds",
+        translation_key="total_volatile_organic_compounds",
+        fallback_name="Total volatile organic compounds",
+    )
+    .write_attr_button(
+        cluster_id=ThirdRealityRadarCluster.cluster_id,
+        attribute_name=ThirdRealityRadarCluster.AttributeDefs.tvoc_sensor_calibration.name,
+        attribute_value=0x01,
+        endpoint_id=1,
+        translation_key="tvoc_sensor_calibration",
+        fallback_name="TVOC sensor calibration",
+    )
+    .number(
+        attribute_name=ThirdRealityRadarCluster.AttributeDefs.sensor_sensitivity.name,
+        min_value=1,
+        max_value=6,
+        step=1,
+        cluster_id=ThirdRealityRadarCluster.cluster_id,
+        translation_key="sensor_sensitivity",
+        fallback_name="Presence sensor sensitivity",
+    )
+    .number(
+        attribute_name=ThirdRealityRadarCluster.AttributeDefs.air_threshold.name,
+        min_value=3000,
+        max_value=15000,
+        step=1,
+        cluster_id=ThirdRealityRadarCluster.cluster_id,
+        translation_key="air_threshold",
+        fallback_name="Air threshold",
     )
     .add_to_registry()
 )
