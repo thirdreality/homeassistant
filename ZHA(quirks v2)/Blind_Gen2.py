82ac5dfd41ad1b5603ba11f6aba2185753f1f230
jintj <jintj@3reality.com>
2026-03-20 09:43:07 +0800

    update

--- ZHA(quirks v2)/Blind_Gen2.py
+++ ZHA(quirks v2)/Blind_Gen2.py
@@ -6,6 +6,7 @@ from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
 import zigpy.types as t
 from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
+from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass, SensorStateClass
 
 
 class THIRD_REALITY_Blind_Gen2_CLUSTER(CustomCluster):
@@ -15,47 +16,47 @@ class THIRD_REALITY_Blind_Gen2_CLUSTER(CustomCluster):
 
     class AttributeDefs(BaseAttributeDefs):
         """Define the attributes of a private cluster."""
-		
-        
+
         enable_disable_pir_remote: Final = ZCLAttributeDef(
             id=0x0000,
             type=t.uint8_t,
             is_manufacturer_specific=True,
         )
-        
-        compensation_speed: Final = ZCLAttributeDef(
-			id=0x0001,
-			type=t.int8s,
-			is_manufacturer_specific=True,
-		)
 
-				
+        compensation_speed: Final = ZCLAttributeDef(
+            id=0x0001,
+            type=t.int8s,
+            is_manufacturer_specific=True,
+        )
+
         limit_position: Final = ZCLAttributeDef(
-			id=0x0002,
-			type=t.uint16_t,
-			is_manufacturer_specific=True,
-		)
+            id=0x0002,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
 
         total_cycle_times: Final = ZCLAttributeDef(
-			id=0x0003,
-			type=t.uint16_t,
-			is_manufacturer_specific=True,
-		)
+            id=0x0003,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
 
         last_remaining_battery_percentage: Final = ZCLAttributeDef(
-			id=0x0004,
-			type=t.uint8_t,
-			is_manufacturer_specific=True,
-		)
+            id=0x0004,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
+
+
 (
     QuirkBuilder("Third Reality, Inc", "3RSB02015Z")
     .replaces(THIRD_REALITY_Blind_Gen2_CLUSTER, endpoint_id=1)
     .switch(
         attribute_name=THIRD_REALITY_Blind_Gen2_CLUSTER.AttributeDefs.enable_disable_pir_remote.name,
         cluster_id=THIRD_REALITY_Blind_Gen2_CLUSTER.cluster_id,
-		force_inverted = True,
-        translation_key="enable_disable_pir_mode",
-        fallback_name="Enable/Disable PIR Remote",
+        force_inverted=True,
+        translation_key="enable_pir_mode",
+        fallback_name="Enable PIR remote",
     )
     .number(
         attribute_name=THIRD_REALITY_Blind_Gen2_CLUSTER.AttributeDefs.compensation_speed.name,
@@ -64,28 +65,21 @@ class THIRD_REALITY_Blind_Gen2_CLUSTER(CustomCluster):
         min_value=-100,
         max_value=100,
         step=1,
-        # mode="box",  # 也可以用"slider"
-        #multiplier=1,
-        #entity_type="number",
-        #device_class="duration",
+        mode="box",
         translation_key="compensation_speed",
-        fallback_name="Compensation_speed",
+        fallback_name="Compensation speed",
     )
     .number(
         attribute_name=THIRD_REALITY_Blind_Gen2_CLUSTER.AttributeDefs.limit_position.name,
         cluster_id=THIRD_REALITY_Blind_Gen2_CLUSTER.cluster_id,
         endpoint_id=1,
         min_value=0,
-        max_value=3800,
+        max_value=4000,
         step=1,
-        # mode="box",  # 也可以用"slider"
-        #multiplier=1,
-        #entity_type="number",
-        #device_class="duration",
         translation_key="limit_position",
-        fallback_name="Limit_position",
+        fallback_name="Limit position",
     )
-	.number(
+    .number(
         attribute_name=THIRD_REALITY_Blind_Gen2_CLUSTER.AttributeDefs.total_cycle_times.name,
         cluster_id=THIRD_REALITY_Blind_Gen2_CLUSTER.cluster_id,
         endpoint_id=1,
@@ -93,7 +87,16 @@ class THIRD_REALITY_Blind_Gen2_CLUSTER(CustomCluster):
         max_value=334,
         step=1,
         translation_key="total_cycle_times",
-        fallback_name="Total_cycle_times",
+        fallback_name="Total cycle times",
+    )
+    .sensor(
+        attribute_name=THIRD_REALITY_Blind_Gen2_CLUSTER.AttributeDefs.last_remaining_battery_percentage.name,
+        cluster_id=THIRD_REALITY_Blind_Gen2_CLUSTER.cluster_id,
+        endpoint_id=1,
+        device_class=SensorDeviceClass.BATTERY,
+        state_class=SensorStateClass.MEASUREMENT,
+        translation_key="last_remaining_battery_percentage",
+        fallback_name="Last remaining battery percentage",
     )
     .add_to_registry()
 )
