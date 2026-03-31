d921a6ba8d40616e5c30de98b52ed6e4a8bb72c3
jintj <jintj@3reality.com>
2026-03-31 10:45:27 +0800

    update air pressure sensor,curtain gen2,air quality sensor

--- ZHA(quirks v2)/Smart Air Pressure Sensor.py
+++ ZHA(quirks v2)/Smart Air Pressure Sensor.py
@@ -1,41 +1,30 @@
 """Third Reality formaldehyde sensor devices."""
 
 from typing import Final
-
 from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import QuirkBuilder, ReportingConfig, SensorDeviceClass, SensorStateClass
+from zigpy.quirks.v2 import QuirkBuilder, SensorDeviceClass, SensorStateClass
 from zigpy.quirks.v2.homeassistant import PERCENTAGE
-from zigpy.zcl.clusters.general import (
-    Basic,
-    Groups,
-    Identify,
-    LevelControl,
-    OnOff,
-    Ota,
-    Scenes,
-    Time,
-)
+from zigpy.zcl.clusters.general import AnalogInput
+from zigpy.zcl.clusters.measurement import PressureMeasurement
+from zigpy.zcl.clusters.general_const import AnalogInputType
+
+class CustomAnalogInputCluster(CustomCluster, AnalogInput):
+    """Custom AnalogInput cluster with modified description and application_type."""
+    
+    _CONSTANT_ATTRIBUTES = {
+        AnalogInput.AttributeDefs.application_type.id: 0x00040000,  # Percentage类型 (0x04), group=0, index=0
+        AnalogInput.AttributeDefs.description.id: "Dirty Level",  # 自定义名称
+    }
 
 
 (
     QuirkBuilder("Third Reality, Inc", "3RAP0149BZ")
-    .prevent_default_entity_creation(
+    .adds(CustomAnalogInputCluster)
+    .change_entity_metadata(
         endpoint_id=1,
-        cluster_id=LevelControl.cluster_id,
-        function=lambda entity: entity.translation_key in [
-            "on_level",
-            "on_off_transition_time",
-            "default_move_rate",
-            "start_up_current_level"
-        ]
-    )
-    .sensor(
-        cluster_id=LevelControl.cluster_id,
-        attribute_name="current_level",
-        device_class=SensorDeviceClass.AQI,
-        state_class=SensorStateClass.MEASUREMENT,
-        unit=PERCENTAGE,
-        fallback_name="Air quality level",
+        cluster_id=PressureMeasurement.cluster_id,
+        new_primary=False,
+        new_fallback_name="Pressure",
     )
     .add_to_registry()
 )
