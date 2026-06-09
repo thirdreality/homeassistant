a806c66b3325b37dded7a7b2d839431a000afd74
support <117250319+3reality-support@users.noreply.github.com>
2026-06-09 18:29:55 +0800

    Add files via upload

---
+++ Z2M/Smart_Vibration_Sensor.js
@@ -0,0 +1,96 @@
+import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
+
+const fzLocal = {
+    thirdreality_acceleration: {
+        cluster: 'THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            //const vibration = msg.data['0'];
+            if (msg.data['0'] == 1)
+			{
+				const payload = {"x_axis": msg.data['x_axis'], "y_axis": msg.data['y_axis'], "z_axis": msg.data['z_axis']};
+				return payload;
+			}
+			//const payload = {};
+			if (msg.data['Xangle']!== undefined)
+			{
+				const payload = {"Xangle": msg.data['Xangle']};
+				return payload;
+			}
+			if (msg.data['y_angle']!== undefined)
+			{
+				const payload = {"y_angle": msg.data['y_angle']};
+				return payload;
+			}
+			if (msg.data['z_angle']!== undefined)
+			{
+				const payload = {"z_angle": msg.data['z_angle']};
+				return payload;
+			}
+        },
+    },
+};
+export default {
+    zigbeeModel: ['3RVS01031Z'],
+    model: '3RVS01031Z',
+    vendor: 'Third Reality',
+    description: 'Zigbee vibration sensor',
+    // fromZigbee: [fzLocal.thirdreality_acceleration],
+    ota: true,
+    // exposes: [e.vibration(), e.battery_low(), e.battery(), e.battery_voltage(), e.x_axis(), e.y_axis(), e.z_axis(), e.angle_axis("Xangle"), e.angle_axis("y_angle"), e.angle_axis("z_angle"),],
+	extend: [
+		m.deviceAddCustomCluster('THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID', {
+			name: "THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID",
+			ID: 0xfff1,
+			manufacturerCode: 0x1233,
+			attributes: {
+				cool_down_time: {name: "cool_down_time", ID: 0x0004, type: 0x21, write: true, max: 7200},
+                x_axis: {name: "x_axis", ID: 0x0001, type: 0x29, write: false, min: -32768},
+                y_axis: {name: "y_axis", ID: 0x0002, type: 0x29, write: false , min: -32768},
+                z_axis: {name: "z_axis", ID: 0x0003, type: 0x29, write: false, min: -32768},
+				x_angle: {name: "x_angle", ID: 0x0005, type: 0x29, write: false, min: -180},
+                y_angle: {name: "y_angle", ID: 0x0006, type: 0x29, write: false, min: -180},
+                z_angle: {name: "z_angle", ID: 0x0007, type: 0x29, write: false, min: -180},
+			},
+			commands: {},
+			commandsResponse: {},
+		}),
+		m.battery(),
+		m.iasZoneAlarm({
+			zoneType: "generic",
+			zoneAttributes: ["alarm_1"],
+			description: "Being in vibration",
+		}),
+		m.numeric({
+			name: "cool_down_time",
+			unit: "s",
+			valueMin: 0,
+			valueMax: 7200,
+			cluster: "THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID",
+			attribute: "cool_down_time",
+			description: "coolDownTime",
+			access: "ALL",
+		}),
+		m.numeric({
+			name: "x_axis",
+			cluster: "THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID",
+			attribute: "x_axis",
+			description: "X axis acceleration",
+			access: "STATE_GET",
+		}),
+		m.numeric({
+			name: "y_axis",
+			cluster: "THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID",
+			attribute: "y_axis",
+			description: "Y axis acceleration",
+			access: "STATE_GET",
+		}),
+		m.numeric({
+			name: "z_axis",
+			cluster: "THIRD_REALITY_VIBRATE_DELAY_CLUSTER_ID",
+			attribute: "z_axis",
+			description: "Z axis acceleration",
+			access: "STATE_GET",
+		}),
+	],
+};
